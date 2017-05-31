a049e44a8a6d60a3ae436412ede2f967dde519c4
SamSi <sam99xxx1@gmail.com>
2017-05-31 18:41:04 +0530

    Update tri-tiling.cpp

--- tri-tiling.cpp
+++ tri-tiling.cpp
@@ -28,3 +28,34 @@ int main(){
         solution();
     }
 }
+
+
+// Better Solution
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solution(){
+    int N;
+    cin >> N;
+
+    int A[9999] = {1};
+    A[2] = 3;
+
+    for(int n = 4; n <= N+2; n++) {
+        A[n] = 4*A[n-2] - A[n-4];
+    }
+
+    cout << A[N] << endl;
+}
+
+int main(){
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        solution();
+    }
+}
